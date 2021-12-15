b30250e5c8631cc4337332a605271a15f3229c84
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-15 15:13:52 +0400

    feat: created Tabs page

--- JS/script.js
+++ JS/script.js
@@ -1,6 +1,36 @@
-$(document).ready(function(){
-    $(".accordion-button").click(function(){
-      $(".plus").hide();
-    });
+$(document).ready(function () {
+
+                // START ACCORDION
+
+  // $(".accordion-button").click(function () {
+  //   $(".plus").addClass("d-none")
+  //   $(".minus").removeClass("d-none")
+  // });
+
+  // $(".accordion-button").dblclick(function () {
+  //   $(".minus").removeClass("d-none")
+  // });
+
+  $(".accordion-button").mouseenter(function(){
+    $(".minus").removeClass("d-none")
+    $(".plus").addClass("d-none")
   });
-  
+
+  $(".accordion-button").mouseleave(function(){
+    $(".minus").addClass("d-none")
+    $(".plus").removeClass("d-none")
+  });
+
+  $(".accordion").accordion();
+
+                  // END ACCORDION
+
+
+                  //START TABS
+// if($( "#nav-home-tab" ).tabs({ active:1})){
+//   ("#nav-home-tab").CSS("background-color", "orangered")
+// }
+// #tabs .ui-tabs-active {
+//   background: #5ba63c;
+// }
+});
