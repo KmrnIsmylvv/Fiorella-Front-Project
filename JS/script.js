4b758ce0cc40a625f71e674a6a0650a25fdeec9f
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-20 23:42:11 +0400

    feat: added navbar all pages

--- JS/script.js
+++ JS/script.js
@@ -1,26 +1,51 @@
 $(document).ready(function () {
+  //START NAVBAR
 
-                // START ACCORDION
-
- 
-
-  $(".accordion-button").mouseenter(function(){
-    $(".minus").removeClass("d-none")
-    $(".plus").addClass("d-none")
+  $(document).on('click', '#search', function () {
+    $(this).next().toggle();
   });
 
-  $(".accordion-button").mouseleave(function(){
-    $(".minus").addClass("d-none")
-    $(".plus").removeClass("d-none")
+  $(document).on('click', '#mobile-navbar-close', function () {
+    $(this).parent().removeClass('active');
+  });
+  $(document).on('click', '#mobile-navbar-show', function () {
+    $('.mobile-navbar').addClass('active');
   });
 
-  $(".accordion").accordion();
+  $(document).on('click', '.mobile-navbar ul li a', function () {
+    if ($(this).children('i').hasClass('fa-caret-right')) {
+      $(this)
+        .children('i')
+        .removeClass('fa-caret-right')
+        .addClass('fa-sort-down');
+    } else {
+      $(this)
+        .children('i')
+        .removeClass('fa-sort-down')
+        .addClass('fa-caret-right');
+    }
+    $(this).parent().next().slideToggle();
+  });
 
-                  // END ACCORDION
+  //END NAVBAR
 
-               
-   $(".card").hover(function(){
-     $(".row .inner").removeClass("d-none")
-   });         
+  // START ACCORDION
 
+  $('.accordion-button').mouseenter(function () {
+    $('.minus').removeClass('d-none');
+    $('.plus').addClass('d-none');
+  });
+
+  $('.accordion-button').mouseleave(function () {
+    $('.minus').addClass('d-none');
+    $('.plus').removeClass('d-none');
+  });
+
+  $('.accordion').accordion();
+
+  // END ACCORDION
+
+  $('.card').hover(function () {
+    $('.row .inner').removeClass('d-none');
+  });
 });
