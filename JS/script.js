94635a7d9368a51433933f713fd47f5f54104dfb
DESKTOP-50U56HO <kamranvi@code.edu.az>
2021-12-22 23:36:58 +0400

    feat: added productlist

--- JS/script.js
+++ JS/script.js
@@ -1,3 +1,28 @@
+// START AMARALLYS
+let product_menus = document.querySelectorAll('#tab_menu .menus .menu');
+let product_contents = document.querySelectorAll(
+  '#tab_menu .contents .content'
+);
+
+for (let menu of product_menus) {
+  menu.addEventListener('click', function () {
+    document
+      .querySelector('#tab_menu .menus .selected')
+      .classList.remove('selected');
+    this.classList.add('selected');
+    let data_id = this.getAttribute('data-id');
+    for (let content of product_contents) {
+      if (data_id == content.getAttribute('data-id')) {
+        content.classList.remove('d-none');
+      } else {
+        content.classList.add('d-none');
+      }
+    }
+  });
+}
+// END AMARLLYS
+
+
 $(document).ready(function () {
   //START NAVBAR
 
@@ -29,33 +54,30 @@ $(document).ready(function () {
 
   //END NAVBAR
 
-
-
   
-// START SLIDE
-$(function() {
-  $(".Slide").vegas({
-      slides: [
-          { src: '../IMG/h3-slider-background.jpg' },
-          { src: '../IMG/h3-slider-background-2.jpg' },
-          { src: '../IMG/h3-slider-background-3.jpg' }
-      ],
-      animation: 'kenburnsDownLeft'
-  });
-});
-// END SLIDE
 
-// START BOTTOM SLIDE
-$(document).ready(function(){
-  $(".bottom-slider").owlCarousel(
-      {
-          items: 1,
-          loop: true,
-          autoplay: true
-      }
-  );
-});
-// END BOTTOM SLIDE
+  // START SLIDE
+  $(function () {
+    $('.Slide').vegas({
+      slides: [
+        { src: '../IMG/h3-slider-background.jpg' },
+        { src: '../IMG/h3-slider-background-2.jpg' },
+        { src: '../IMG/h3-slider-background-3.jpg' },
+      ],
+      animation: 'kenburnsDownLeft',
+    });
+  });
+  // END SLIDE
+
+  // START BOTTOM SLIDE
+  $(document).ready(function () {
+    $('.bottom-slider').owlCarousel({
+      items: 1,
+      loop: true,
+      autoplay: true,
+    });
+  });
+  // END BOTTOM SLIDE
 
   // START ACCORDION
 
@@ -77,11 +99,6 @@ $(document).ready(function(){
     $('.row .inner').removeClass('d-none');
   });
 
-
-
-
-
-
-
-
+  
 });
+
